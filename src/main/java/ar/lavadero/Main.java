af5fd11c4aeeba5b7bf5037baae00bbe6b022760
Gonzalo Sola <gfsola@afip.gob.ar>
2019-01-31 12:22:33 -0300

    Cambios

--- src/main/java/ar/lavadero/Main.java
+++ src/main/java/ar/lavadero/Main.java
@@ -1,14 +1,17 @@
-package ar.lavadero.cuenca;
+package ar.lavadero;
 
 import javax.swing.*;
 import java.awt.*;
+//import org.slf4j.*;
 
 public class Main extends JApplet {
+
+    //public static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         JFrame f = new JFrame("Aplicación");
         Main ta = new Main();
-
+      //  logger.info("entre");
         f.getContentPane().setLayout(new BorderLayout());
         f.getContentPane().add("Center", ta);
         int width = 400;
