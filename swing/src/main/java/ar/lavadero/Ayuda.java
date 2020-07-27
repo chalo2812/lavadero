6bf858b5086116c83d9a9835b22c1c1c6171f347
Gonzalo <gonzalosola@gmail.com>
2020-07-27 02:18:31 -0300

    Re ubicacion

--- swing/src/main/java/ar/lavadero/Ayuda.java
+++ swing/src/main/java/ar/lavadero/Ayuda.java
@@ -1,15 +1,10 @@
 package ar.lavadero;
 
 import ar.lavadero.components.Cuadro;
-import org.slf4j.*;
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
-import java.net.*;
 
 public class Ayuda extends Cuadro {
-
-    public static Logger logger = LoggerFactory.getLogger(Ayuda.class);
 
     Ayuda(String titulo){
         super(titulo);
@@ -18,7 +13,6 @@ public class Ayuda extends Cuadro {
     public Cuadro crearAyuda(Cuadro f){
         f.setExtendedState(JFrame.NORMAL);
         Dimension ubicacion = Toolkit.getDefaultToolkit().getScreenSize();
-        logger.info("height es " + ubicacion.height + ", width es " + ubicacion.width);
         f.setLocation(ubicacion.width/2 - this.getSize().width/2, ubicacion.height/2 - this.getSize().height/2);
         f.setSize(400,400);
 
