5ef3b1ff57f4a5ec40e75e34ed48a6c6e9df7f85
Gonzalo <gonzalosola@gmail.com>
2020-07-27 02:31:47 -0300

    Springboot ejecutable

---
+++ server/src/main/java/ar/lavadero/spring/LavaderoRunner.java
@@ -0,0 +1,13 @@
+package ar.lavadero.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaderoRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavaderoRunner.class, args);
+    }
+
+}
