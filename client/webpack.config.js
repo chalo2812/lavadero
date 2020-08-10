f1fe4b50696c07cc25d63cf94a31095bb3a06a28
Gonzalo Sola <gonzalosola@gmail.com>
2020-08-10 02:00:41 -0300

    Configuracion de WebPack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,34 @@
-'use strict';
-
-const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-
+	module: {
+    rules: [
+    {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: {
+        hotReload: true
+      }
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: {
+        hotReload: true
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        'css-loader'
+      ],
+      options: {
+        hotReload: true
+      }
+    }
+  ]},
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 };
