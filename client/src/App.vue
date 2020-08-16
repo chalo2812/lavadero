9cf7fa610313e546d5dbd70919e1df6517469c21
Gonzalo Sola <gonzalosola@gmail.com>
2020-08-16 02:49:17 -0300

    Prueba con componentes dentro de otros.

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,6 @@
 			<b-navbar-brand href="#">
 				<b-avatar variant="secondary"></b-avatar>
 			</b-navbar-brand>
-
 			<b-collapse id="nav-collapse" is-nav>
 				<b-navbar-nav class="ml-auto">
 					<b-img thumbnail fluid src="https://picsum.photos/64/64/?image=1"></b-img>
@@ -19,13 +18,19 @@
 				</b-navbar-nav>
 			</b-collapse>
 		</b-navbar>
+		<Home ></Home>
 	</div>
+
 </template>
 
 <script>
+import Home from './components/Home.vue'
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Home
+  }
 }
 </script>
 
@@ -36,5 +41,30 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.container {
+  display: inline-block;
+  cursor: pointer;
+}
+
+.bar1, .bar2, .bar3 {
+  width: 35px;
+  height: 5px;
+  background-color: #333;
+  margin: 6px 0;
+  transition: 0.4s;
+}
+
+.change .bar1 {
+  -webkit-transform: rotate(-45deg) translate(-9px, 6px);
+  transform: rotate(-45deg) translate(-9px, 6px);
+}
+
+.change .bar2 {opacity: 0;}
+
+.change .bar3 {
+  -webkit-transform: rotate(45deg) translate(-8px, -8px);
+  transform: rotate(45deg) translate(-8px, -8px);
 }
 </style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+	<p>Body</p>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
